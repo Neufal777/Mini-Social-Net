69d99825404e8e0224e42eb3161f75373c7d79b7
Naoufal Dahouli <neufal777@gmail.com>
2018-10-19 22:18:20 +0200

    simple comment

--- decline_friend.php
+++ decline_friend.php
@@ -1,10 +1,11 @@
 <?php
-	
+
 	session_start();
 	include 'php/connection.php';
 
 	$friend_request_id = $_GET['id'];
 
+	//Decline friend, database sql
 	mysqli_query($db_con,"UPDATE friends SET status='declined' where id='$friend_request_id' ");
 	header("location: home.php");
 ?>
